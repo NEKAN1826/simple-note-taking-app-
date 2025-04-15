45cfd48b1b027928065a512caff9164ca42ffc29
Robe ;) <ottink.roberto@gmail.com>
2025-04-15 17:01:20 +0200

    added Styling
    CSS Styling hinzugefügt.
    Kleinen Fehler im java script behoben, der dazu geführt hat, dass für jeden Eintrag zwei <li> Elemente erstellt wurden.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function addNote(){
     let note = document.getElementById('noteInput').value;
     if (note != ""){
         let listItem = document.createElement("li");
-        listItem.innerHTML = `<li>${note}</li>`;
+        listItem.innerHTML = `${note}`;
         document.getElementById("notebook").append(listItem);
         document.getElementById('noteInput').value = "";
     }
