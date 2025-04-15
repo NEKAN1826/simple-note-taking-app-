df49a0d1b21fac5ded55d10b312e0951ad190274
Robe ;) <ottink.roberto@gmail.com>
2025-04-15 12:44:09 +0200

    Basic functionality
    Habe das Grundgerüst der Seite fertiggestellt.

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("noteSubmit").onclick = function(){addNote()};
+function addNote(){
+    let note = document.getElementById('noteInput').value;
+    if (note != ""){
+        let listItem = document.createElement("li");
+        listItem.innerHTML = `<li>${note}</li>`;
+        document.getElementById("notebook").append(listItem);
+    }
+}
